bd3ac1e96ac6c36b4cc8065b7776255550e0f9ea
KitanovskiD <dimitar.kitanovski@outlook.com>
2023-03-06 09:42:26 +0100

    First Auditory Exercise

---
+++ EShop/EShop.Web/EShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using EShop.Web.Models.DomainModels;
+using EShop.Web.Models.Relationship;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public virtual DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+               .Property(z => z.Id)
+               .ValueGeneratedOnAdd();
+
+            builder.Entity<ShoppingCart>()
+                .Property(z => z.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Entity<ProductInShoppingCart>().HasKey(psc => new { psc.ProductId, psc.ShoppingCartId});
+
+            builder.Entity<ProductInShoppingCart>()
+                .HasOne(p => p.Product)
+                .WithMany(sc => sc.ProductInShoppingCarts)
+                .HasForeignKey(z => z.ShoppingCartId);
+
+            builder.Entity<ProductInShoppingCart>()
+              .HasOne(sc => sc.ShoppingCart)
+              .WithMany(sc => sc.ProductInShoppingCarts)
+              .HasForeignKey(z => z.ProductId);
+        }
+    }
+}
---
+++ EShop/EShop.Web/EShop.Web/Models/Relationship/ProductInShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Web.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Web.Models.Relationship
+{
+    public class ProductInShoppingCart
+    {
+        public Guid ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public Guid ShoppingCartId { get; set; }
+        public virtual ShoppingCart ShoppingCart { get; set; }
+    }
+}
